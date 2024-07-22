0707fd469fcd05602682b8877fcec3575c6d267f
김주연 <zpffjtm@naver.com>
2024-07-22 14:56:08 +0900

    main screen ui

--- app/src/main/java/com/example/greenaction/MainActivity.java
+++ app/src/main/java/com/example/greenaction/MainActivity.java
@@ -1,42 +1,77 @@
 package com.example.greenaction;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 import androidx.drawerlayout.widget.DrawerLayout;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 import com.google.android.material.navigation.NavigationView;
-import android.os.Build;
-import android.view.Window;
-import android.view.WindowManager;
 
 public class MainActivity extends AppCompatActivity {
 
     private DrawerLayout drawerLayout;
-    private ActionBarDrawerToggle toggle;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Toolbar toolbar = findViewById(R.id.toolbar);
+        toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         drawerLayout = findViewById(R.id.drawer_layout);
         NavigationView navigationView = findViewById(R.id.nav_view);
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 
-        toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        // 상태 표시줄 색상 변경
+        if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.LOLLIPOP) {
+            getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        }
+
+        // 네비게이션 드로어 토글 설정
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawerLayout, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
         drawerLayout.addDrawerListener(toggle);
         toggle.syncState();
 
-        // 상태 표시줄 색상 변경
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
-            Window window = getWindow();
-            window.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS);
-            window.setStatusBarColor(getResources().getColor(R.color.colorPrimary));
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            int id = item.getItemId();
+            if (id == R.id.navigation_home) {
+                // Home 화면으로 이동
+                return true;
+            } else if (id == R.id.navigation_dashboard) {
+                // Dashboard 화면으로 이동
+                return true;
+            } else if (id == R.id.navigation_notifications) {
+                // Notifications 화면으로 이동
+                return true;
+            }
+            return false;
+        });
+    }
+
+    public void onPartitionClick(View view) {
+        Intent intent;
+        int id = view.getId();
+        if (id == R.id.air_pollution) {
+            intent = new Intent(this, AirPollutionActivity.class);
+        } else if (id == R.id.water_pollution) {
+            intent = new Intent(this, WaterPollutionActivity.class);
+        } else if (id == R.id.soil_pollution) {
+            intent = new Intent(this, SoilPollutionActivity.class);
+        } else if (id == R.id.plastic_pollution) {
+            intent = new Intent(this, PlasticPollutionActivity.class);
+        } else {
+            return;
         }
+        startActivity(intent);
+    }
+
+    public void onMoreClick(View view) {
+        // "더 보기" 클릭 시 실행할 코드 작성
     }
 }
---
+++ app/src/main/java/com/example/greenaction/WaterPollutionActivity.java
@@ -0,0 +1,12 @@
+package com.example.greenaction;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class WaterPollutionActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_water_pollution);
+    }
+}
