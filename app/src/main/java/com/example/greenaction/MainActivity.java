cb0dba12354c5b6541328f75c5475f32395356d7
BURRY1119 <burry1119@gmail.com>
2024-07-25 16:51:30 +0900

    UI update

--- app/src/main/java/com/example/greenaction/MainActivity.java
+++ app/src/main/java/com/example/greenaction/MainActivity.java
@@ -3,7 +3,6 @@ package com.example.greenaction;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBarDrawerToggle;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
